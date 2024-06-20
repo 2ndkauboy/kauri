26cac26e0d38b51af7e33df4a2a84c664315b528
Bernhard Kau <github@kau-boys.de>
2024-06-20 14:22:20 +0200

    Set different styles for search in header and sidebar

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -2,34 +2,34 @@
 /**
  * Title: Sidebar
  * Slug: kauri/sidebar
+ * Categories: hidden
  * Inserter: no
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background-secondary-background-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px">
-	<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+<div class="wp-block-group has-background-secondary-background-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
 
-	<!-- wp:heading {"level":3} -->
+<!-- wp:heading {"level":3} -->
 	<h3 class="wp-block-heading"><?php esc_html_e( 'Recent Posts', 'kauri' ); ?></h3>
-	<!-- /wp:heading -->
+<!-- /wp:heading -->
 
-	<!-- wp:latest-posts /-->
+<!-- wp:latest-posts /-->
 
-	<!-- wp:heading {"level":3} -->
+<!-- wp:heading {"level":3} -->
 	<h3 class="wp-block-heading"><?php esc_html_e( 'Recent Comments', 'kauri' ); ?></h3>
-	<!-- /wp:heading -->
+<!-- /wp:heading -->
 
-	<!-- wp:latest-comments {"displayAvatar":false,"displayDate":false,"displayExcerpt":false} /-->
+<!-- wp:latest-comments {"displayAvatar":false,"displayDate":false,"displayExcerpt":false} /-->
 
-	<!-- wp:heading {"level":3} -->
+<!-- wp:heading {"level":3} -->
 	<h3 class="wp-block-heading"><?php esc_html_e( 'Categories', 'kauri' ); ?></h3>
-	<!-- /wp:heading -->
+<!-- /wp:heading -->
 
-	<!-- wp:categories /-->
+<!-- wp:categories /-->
 
-	<!-- wp:heading {"level":3} -->
+<!-- wp:heading {"level":3} -->
 	<h3 class="wp-block-heading"><?php esc_html_e( 'Archives', 'kauri' ); ?></h3>
-	<!-- /wp:heading -->
+<!-- /wp:heading -->
 
-	<!-- wp:archives /--></div>
+<!-- wp:archives /--></div>
 <!-- /wp:group -->
