7f2d6705a96c034d705569e25b9bc85c372c6c91
Bernhard Kau <github@kau-boys.de>
2024-06-20 16:43:29 +0200

    Style "widgets" in the sidebar

--- patterns/sidebar.php
+++ patterns/sidebar.php
@@ -1,35 +1,45 @@
 <?php
 /**
- * Title: Sidebar
+ * Title: sidebar
  * Slug: kauri/sidebar
  * Categories: hidden
  * Inserter: no
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-background-secondary-background-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"backgroundColor":"background-secondary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background-secondary-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php esc_html_e( 'Recent Posts', 'kauri' ); ?></h3>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html__('Recent Posts', 'kauri');?></h3>
 <!-- /wp:heading -->
 
-<!-- wp:latest-posts /-->
+<!-- wp:latest-posts {"style":{"typography":{"lineHeight":"1.6"}}} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php esc_html_e( 'Recent Comments', 'kauri' ); ?></h3>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html__('Recent Comments', 'kauri');?></h3>
 <!-- /wp:heading -->
 
-<!-- wp:latest-comments {"displayAvatar":false,"displayDate":false,"displayExcerpt":false} /-->
+<!-- wp:latest-comments {"displayAvatar":false,"displayDate":false,"displayExcerpt":false} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php esc_html_e( 'Categories', 'kauri' ); ?></h3>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html__('Categories', 'kauri');?></h3>
 <!-- /wp:heading -->
 
-<!-- wp:categories /-->
+<!-- wp:categories /--></div>
+<!-- /wp:group -->
 
-<!-- wp:heading {"level":3} -->
-	<h3 class="wp-block-heading"><?php esc_html_e( 'Archives', 'kauri' ); ?></h3>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading"><?php echo esc_html__('Archives', 'kauri');?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:archives /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
